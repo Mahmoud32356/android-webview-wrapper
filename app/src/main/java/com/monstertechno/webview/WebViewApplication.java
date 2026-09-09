43c96a43ebb87ec246d065a8df566e2f20b89b06
Mahmoud32356 <mahmoud27457@gmail.com>
2026-09-09 22:11:25 +0300

    Update WebViewApplication.java

--- app/src/main/java/com/monstertechno/webview/WebViewApplication.java
+++ app/src/main/java/com/monstertechno/webview/WebViewApplication.java
@@ -12,6 +12,10 @@ import androidx.work.WorkManager;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import androidx.annotation.NonNull;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 import com.monstertechno.webview.core.WebViewManager;
 import com.monstertechno.webview.utils.NotificationChannels;
